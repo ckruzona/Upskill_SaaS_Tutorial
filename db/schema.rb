801c0a7c716d6e616e0269770a427104839e1db9
Chris Kruzona <chris_kruzona@hotmail.com>
2017-01-11 04:24:51 +0000

    Added create action to save to db and fixed migration/git

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170106034646) do
+ActiveRecord::Schema.define(version: 20170111040249) do
 
   create_table "contacts", force: :cascade do |t|
     t.string   "name"
